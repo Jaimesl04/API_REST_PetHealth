c3d4d6f61652581e6541988fedafbafffc49c024
Jaimesl04 <jaime.de.luengo.al@iespoligonosur.org>
2025-12-17 09:31:59 +0100

    prueba

---
+++ apirestpethealth/src/main/java/com/apirestpethealth/apirestpethealth/ApirestpethealthApplication.java
@@ -0,0 +1,13 @@
+package com.apirestpethealth.apirestpethealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestpethealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestpethealthApplication.class, args);
+	}
+
+}
---
+++ apirestpethealth/src/test/java/com/apirestpethealth/apirestpethealth/ApirestpethealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestpethealth.apirestpethealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestpethealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
